90b6b180c0cbd95e11a6895efabed4e0e8c88554
Sandy Porto <sandyporto@gmail.com>
2014-07-30 15:07:51 -0300

    Trophy storage

--- Gamefication/src/com/esfinge/gamification/mechanics/database/TrophyStorage.java
+++ Gamefication/src/com/esfinge/gamification/mechanics/database/TrophyStorage.java
@@ -9,39 +9,38 @@ import java.util.Map;
 
 import com.esfinge.gamification.achievement.Achievement;
 import com.esfinge.gamification.achievement.Point;
-import com.esfinge.gamification.achievement.Reward;
+import com.esfinge.gamification.achievement.Trophy;
 
-public class RewardStorage {
+public class TrophyStorage {
 	private Connection connection;
 
-	public RewardStorage(Connection c) {
+	public TrophyStorage(Connection c) {
 		connection = c;
 	}
 
-	public void insert(Object user, Reward r) throws SQLException {
+	public void insert(Object user, Trophy t) throws SQLException {
 
 		PreparedStatement stmt;
 		stmt = connection
-				.prepareStatement("insert into gamification.reward "
-						+ "(userid, name, used) values (?,?,?)");
+				.prepareStatement("insert into gamification.trophy "
+						+ "(userid, name) values (?,?)");
 		stmt.setString(1, user.toString());
-		stmt.setString(2, r.getName());
-		stmt.setBoolean(3, r.isUsed());
+		stmt.setString(2, t.getName());
+	
 		stmt.execute();
 	}
 
-	public Reward select(Object user, String name) throws SQLException {
+	public Trophy select(Object user, String name) throws SQLException {
 		PreparedStatement stmt;
 		stmt = connection
-				.prepareStatement("select * from gamification.reward "
+				.prepareStatement("select * from gamification.trophy "
 						+ "where userid=? and name = ?");
 		stmt.setString(1, user.toString());
 		stmt.setString(2, name);
 		ResultSet rs = stmt.executeQuery();
 		if (rs.next()) {
-			boolean u = rs.getBoolean("used");
-			Reward r = new Reward(name, u);
-			return r;
+			Trophy t = new Trophy(name);
+			return t;
 		}
 		return null;
 	}
@@ -50,28 +49,17 @@ public class RewardStorage {
 		Map<String, Achievement> map = new HashMap<String, Achievement>();
 		PreparedStatement stmt;
 		stmt = connection
-				.prepareStatement("select * from gamification.reward "
+				.prepareStatement("select * from gamification.trophy "
 						+ "where userid=?");
 		stmt.setString(1, user.toString());
 		ResultSet rs = stmt.executeQuery();
 		while(rs.next()){
 			String name = rs.getString("name");
-			boolean u = rs.getBoolean("used");
-			Reward r = new Reward(name,u);
-			map.put(r.getName(), r);
+			Trophy t = new Trophy(name);
+			map.put(t.getName(), t);
 		}
 		
 		return map;
 	}
 
-	public void update(Object user, Reward r) throws SQLException {
-		PreparedStatement stmt;
-		stmt = connection
-				.prepareStatement("update gamification.reward "
-						+ "set used = ? where userid=? and name=?");
-		stmt.setString(2, user.toString());
-		stmt.setString(3, r.getName());
-		stmt.setBoolean(1, r.isUsed());
-		stmt.execute();
-	}
 }
