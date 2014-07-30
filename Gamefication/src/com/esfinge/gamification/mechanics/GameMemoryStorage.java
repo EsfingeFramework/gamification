f6137c7fcc8113b59058b92e360e3fe714f308dd
Sandy Porto <sandyporto@gmail.com>
2014-07-30 11:05:47 -0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    
    	Gamefication/bin/com/esfinge/gamefication/mechanics/TestGameToDatabase.class
    
    	Gamefication/bin/com/esfinge/gamification/mechanics/GameDatabaseStorage.class

--- Gamefication/src/com/esfinge/gamification/mechanics/GameMemoryStorage.java
+++ Gamefication/src/com/esfinge/gamification/mechanics/GameMemoryStorage.java
@@ -1,4 +1,5 @@
 package com.esfinge.gamification.mechanics;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -6,84 +7,73 @@ import java.util.Map;
 import com.esfinge.gamification.achievement.Achievement;
 import com.esfinge.gamification.listener.AchievementListener;
 
+public class GameMemoryStorage extends Game {
 
-public class GameMemoryStorage implements Game {
+	private Map<Object, Map<String, Achievement>> achievments = new HashMap<>();
 
-	private Map<Object,Map<String, Achievement>> achievments = new HashMap<>();
-	private ArrayList<AchievementListener> ac = new ArrayList<>();
-
-	
-	/* (non-Javadoc)
-	 * @see com.esfinge.gamefication.mechanics.Game#addAchievement(java.lang.Object, com.esfinge.gamefication.achievement.Achievement)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.esfinge.gamefication.mechanics.Game#addAchievement(java.lang.Object,
+	 * com.esfinge.gamefication.achievement.Achievement)
 	 */
 	@Override
-	public void addAchievement(Object user, Achievement a){
-		if(!achievments.containsKey(user)){
+	public void insertAchievement(Object user, Achievement a) {
+		if (!achievments.containsKey(user)) {
 			achievments.put(user, new HashMap<String, Achievement>());
 		}
 		Map<String, Achievement> userAchiev = achievments.get(user);
-		if(userAchiev.containsKey(a.getName())){
+		if (userAchiev.containsKey(a.getName())) {
 			userAchiev.get(a.getName()).incrementAchievement(a);
-		}else{
+		} else {
 			userAchiev.put(a.getName(), a);
-			for(AchievementListener listener:ac)
-				listener.achievementAdded(user, a);
 		}
 	}
-	
-	/* (non-Javadoc)
-	 * @see com.esfinge.gamefication.mechanics.Game#removeAchievement(java.lang.Object, com.esfinge.gamefication.achievement.Achievement)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.esfinge.gamefication.mechanics.Game#removeAchievement(java.lang.Object
+	 * , com.esfinge.gamefication.achievement.Achievement)
 	 */
 	@Override
-	public void removeAchievement(Object user, Achievement a){
-		
-		if(!achievments.containsKey(user)){
+	public void deleteAchievement(Object user, Achievement a) {
+
+		if (!achievments.containsKey(user)) {
 			achievments.put(user, new HashMap<String, Achievement>());
 		}
 		Map<String, Achievement> userAchiev = achievments.get(user);
-		if(userAchiev.containsKey(a.getName())){
+		if (userAchiev.containsKey(a.getName())) {
 			userAchiev.get(a.getName()).removeAchievement(a);
-		}else{
+		} else {
 			userAchiev.put(a.getName(), a);
-			for(AchievementListener listener:ac)
-				listener.achievementRemoved(user, a);
 		}
 	}
-	
-	
-	/* (non-Javadoc)
-	 * @see com.esfinge.gamefication.mechanics.Game#getAchievement(java.lang.Object, java.lang.String)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.esfinge.gamefication.mechanics.Game#getAchievement(java.lang.Object,
+	 * java.lang.String)
 	 */
 	@Override
-	public Achievement getAchievement(Object user, String achievName){
-		
+	public Achievement getAchievement(Object user, String achievName) {
+
 		return achievments.get(user).get(achievName);
 	}
-	
 
-	/* (non-Javadoc)
-	 * @see com.esfinge.gamefication.mechanics.Game#getAchievements(java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.esfinge.gamefication.mechanics.Game#getAchievements(java.lang.Object)
 	 */
 	@Override
-	public Map<String, Achievement> getAchievements(Object user){
+	public Map<String, Achievement> getAchievements(Object user) {
 		return achievments.get(user);
 	}
-	
-	
-	/* (non-Javadoc)
-	 * @see com.esfinge.gamefication.mechanics.Game#addListener(com.esfinge.gamefication.listener.AchievementListener)
-	 */
-	@Override
-	public void addListener(AchievementListener listener){
-		ac.add(listener);
-	}
-	
-	private void notify(AchievementListener listener){
-		for(AchievementListener a : ac){
-		a.achievementAdded(a, null);
-		
-			}
-		}
-	}
-	
 
+}
