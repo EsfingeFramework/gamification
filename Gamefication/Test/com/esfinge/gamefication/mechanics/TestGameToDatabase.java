17ecd1c1e430cd9f25e2ea7e36c871cd22134202
Sandy Porto <sandyporto@gmail.com>
2014-07-31 10:22:06 -0300

    Recolocando os arquivos que foram apagados

--- Gamefication/Test/com/esfinge/gamefication/mechanics/TestGameToDatabase.java
+++ Gamefication/Test/com/esfinge/gamefication/mechanics/TestGameToDatabase.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 
 import com.esfinge.gamification.achievement.Achievement;
 import com.esfinge.gamification.achievement.Point;
+import com.esfinge.gamification.achievement.Ranking;
 import com.esfinge.gamification.achievement.Reward;
 import com.esfinge.gamification.achievement.Trophy;
 import com.esfinge.gamification.mechanics.Game;
@@ -37,6 +38,60 @@ public class TestGameToDatabase {
 		Statement s = connection.createStatement();
 		s.execute("truncate table gamefication.points");
 	    user = "Jaspion";
+	}
+	
+	@Test
+	public void addRanking(){
+		Achievement r = new Ranking("mago", "master");
+		game.addAchievement(user, r);
+		assertEquals(r, (Ranking) game.getAchievement(user, "mago"));
+	}
+	
+	@Test
+	public void addTwoRank() {
+		Achievement r1 = new Ranking("mago", "noob");
+		Achievement r2 = new Ranking("mago", "master");		
+		game.addAchievement("Spider", r1);
+		game.addAchievement("Spider", r2);	
+		assertEquals(r2, (Ranking) game.getAchievement("Spider", "mago"));
+		//assertEquals(1, game.getAchievements("Spider").size());
+		
+	}
+	
+	@Test
+	public void addRankDifferentUser(){
+		FakeUser user2 = new FakeUser("Jiraia");
+		Achievement r1 = new Ranking("mago", "master");
+		Achievement r2 = new Ranking("mago2", "noob");
+		game.addAchievement("Spider", r1);
+		game.addAchievement(user2, r2);
+		//assertEquals(1, game.getAchievements("Spider").size());
+		//assertEquals(1, game.getAchievements(user2).size());
+		assertEquals(r1, (Ranking) game.getAchievement("Spider", "mago"));
+		assertEquals(r2, (Ranking) game.getAchievement(user2, "mago2"));
+	}
+	
+	@Test
+	public void removeRank(){
+		Achievement r = new Ranking("mago", "master");
+		game.addAchievement("Spider", r);
+		game.removeAchievement("Spider", r);	
+		assertEquals(null, (Ranking) game.getAchievement("Spider", "mago"));
+	}
+	
+	@Test
+	public void removeRankDifferentUser(){
+		FakeUser user2 = new FakeUser("Jiraia");
+		Achievement r1 = new Ranking("mago", "master");
+		Achievement r2 = new Ranking("mago2", "noob");
+		game.addAchievement("Spider", r1);
+		game.addAchievement(user2, r2);
+		game.removeAchievement(user2, r1);
+		//assertEquals(1, game.getAchievements("Spider").size());
+		//assertEquals(0, game.getAchievements(user2).size());
+		assertEquals(r1, (Ranking) game.getAchievement("Spider", "mago"));
+		assertEquals(null, (Ranking) game.getAchievement(user2, "mago2"));
+		
 	}
 	
 	@Test
