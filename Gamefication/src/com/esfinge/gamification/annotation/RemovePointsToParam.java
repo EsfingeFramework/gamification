ee7d1c998d4f95c3ab34bc28303249aa633e98bb
Jeremias Ponciano da Silva <jere_2001@ig.com.br>
2015-10-19 16:16:50 -0200

    add RemovePointsToParam.java and RemovePintsToParameterProcessor.java

---
+++ Gamefication/src/com/esfinge/gamification/annotation/RemovePointsToParam.java
@@ -0,0 +1,18 @@
+package com.esfinge.gamification.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.esfinge.gamification.processors.RemovePointsToParameterProcessor;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@GamificationProcessor(RemovePointsToParameterProcessor.class)
+public @interface RemovePointsToParam {
+	int quantity();
+	String name();
+	String param();
+	String prop() default "";
+}
